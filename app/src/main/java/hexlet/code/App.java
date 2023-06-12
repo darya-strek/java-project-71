af93d2aa5bcea8ee94f6f3d10ad8481f01ca4c2d
darya-strekalovskaya <darya.strek@gmail.com>
2023-06-13 00:51:26 +0300

    Add Makefile and fix App.java and build.gradle with picocli dependencies

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,34 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
+
+//    @Option(names = {"-h", "--help"}, description = "Show this help message and exit.")
+//    private boolean help;
+
+//    @Option(names = {"-V", "--version"}, description = "Print version information and exit.")
+//    private boolean version;
+
+    @Override
+    public Integer call() {
+        System.out.printf("Hello!\n");
+        return 0;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
