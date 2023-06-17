40338f8a2f85c545c1ae956eb0f3bde72755c77d
darya-strekalovskaya <darya.strek@gmail.com>
2023-06-18 02:05:38 +0300

    Fix App.java and Differ.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,11 +5,11 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-import java.io.File;
-import java.io.IOException;
-import java.math.BigInteger;
-import java.nio.file.Files;
-import java.security.MessageDigest;
+//import java.io.File;
+//import java.io.IOException;
+//import java.math.BigInteger;
+//import java.nio.file.Files;
+//import java.security.MessageDigest;
 import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
@@ -21,7 +21,8 @@ public class App implements Callable<Integer> {
     @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
     private String filepath2;
 
-    @Option(names = {"-f", "--format"}, defaultValue = "stylish", paramLabel = "format", description = "output format [default: stylish]")
+    @Option(names = {"-f", "--format"}, defaultValue = "stylish", paramLabel = "format",
+            description = "output format [default: stylish]")
     private String format = "stylish";
 
 //    @Option(names = {"-h", "--help"}, description = "Show this help message and exit.")
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -3,11 +3,16 @@ package hexlet.code;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.IOException;
+//import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.Map;
+import java.util.Objects;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.Set;
+import java.util.LinkedHashMap;
 import java.util.stream.Collectors;
 
 public class Differ {
@@ -21,8 +26,8 @@ public class Differ {
 
 
         ObjectMapper mapper = new ObjectMapper();
-        Map<String, Object> data1 = mapper.readValue(file1, new TypeReference<TreeMap<String,Object>>(){});
-        Map<String, Object> data2 = mapper.readValue(file2, new TypeReference<TreeMap<String,Object>>(){});
+        Map<String, Object> data1 = mapper.readValue(file1, new TypeReference<TreeMap<String, Object>>() { });
+        Map<String, Object> data2 = mapper.readValue(file2, new TypeReference<TreeMap<String, Object>>() { });
 
         Set<String> keys = new TreeSet<>();
         keys.addAll(data1.keySet());
