7bc4ca4205d38de2023f088fbf58f2856fc67bd2
darya-strekalovskaya <darya.strek@gmail.com>
2023-07-07 02:01:40 +0300

    Add Stylish.java for make format

---
+++ app/src/main/java/hexlet/code/Stylish.java
@@ -0,0 +1,29 @@
+package hexlet.code;
+
+import java.util.Map;
+
+public class Stylish {
+    public static String format(Map<String, Map<String, Object>> diffMap) {
+        StringBuilder result = new StringBuilder();
+        result.append("{\n");
+
+        for (String key : diffMap.keySet()) {
+            Map<String, Object> map = diffMap.get(key);
+            if (map.get("status").equals("deleted")) {
+                result.append("  - ").append(key).append(": ").append(map.get("value")).append("\n");
+            } else if (map.get("status").equals("added")) {
+                result.append("  + ").append(key).append(": ").append(map.get("value")).append("\n");
+            } else if (map.get("status").equals("changed")) {
+                result.append("  - ").append(key).append(": ").append(map.get("oldValue")).append("\n");
+                result.append("  + ").append(key).append(": ").append(map.get("newValue")).append("\n");
+            } else if (map.get("status").equals("unchanged")) {
+                result.append(" ".repeat(4)).append(key).append(": ").append(map.get("value")).append("\n");
+            } else {
+                throw new RuntimeException();
+            }
+        }
+        result.append("}");
+
+        return result.toString();
+    }
+}
